e55c10654ae1ad1f8c1908fed490e57795e7331f
EmelyS3 <SCONGAS@YAHOO.COM>
2019-01-06 09:45:45 -0400

    listo, pero no muestra el usuario completamente

--- checker.sh
+++ checker.sh
@@ -1,19 +1,369 @@
 #!/bin/bash
 
 
-POINTER=
+# Globals variables user_found_sudoers()
+POINTER_SUDO_GROUPS=
 RET_FOUND_SUDOERS_FILE=
-FILTER_ACCOUNT_NAME="roo*"
+
+
+
+# Filter name process account
+FILTER_ACCOUNT_NAME=
+
+
+
+# Globals variables used by order_procs()
 USER=
 PROCS=
-PID=
+PIDS=
+
+USER2=
+PROCS2=
+PIDS2=
+
+LEN_PROCS=
+
+
+
+# Global variable used by ......
+USER_EXIST=
+
+
+# CSV Output file
+CSV_FILE_OUT="/tmp/chk_sudo_enviroment.csv"
+
+
+
+# Globals variables used by chk_sudo_by_home()
+SUDO_USERS_FOUND=
+QUANTITY_SUDO_USERS=0
+
+
+
+
+# Globals variables used by extract_group_sudoers()
+GROUPS_SUDO_FOUND=
+QUANTITY_SUDO_GROUPS=0
+
+
+#Sudoers folders
+SUDOERS_FOLDERS[0]="/etc/sudoers.d/"
+#SUDOERS_FOLDERS[1]="/etc/group/"
+
+
+#Globals variables for command line
+param_group=0
+param_users=0
+param_privileges=0
+param_help=0
+param_output=0
+
+
+# Next variables
+next_user_filter=0
+next_filename=0
+argc=0
+
+
+
+
+
+function parse_command_line()
+{
+		
+
+
+
+	total_headers=	
+
+
+	if [ $param_help -eq 1 -a \( $total_flags = 1 \) ]
+	then    	
+
+		print_help
+		exit
+
+	fi
+
+
+
+	if [ $param_users -eq 1 ]
+	then
+
+		total_headers="pid,proc_name,user_priv"
+		running_process_by_user $FILTER_ACCOUNT_NAME
+
+	fi
+
+
+
+
+	if [ $param_privileges -eq 1 ]
+	then
+
+		
+		theaders_size=${#total_headers}
+
+		
+		if (( $theaders_size > 0 ))
+		then
+			total_headers=$total_headers","
+		fi
+
+
+		total_headers=$total_headers"home_sudo_privileges"
+		chk_sudo_by_home "/home/"
+
+	fi
+
+
+
+
+	if [ $param_group -eq 1 ]
+	then
+
+
+
+		theaders_size=${#total_headers}
+		#echo $theaders_size
+		#exit
+
+		
+		if (( $theaders_size > 0 )); then
+			total_headers=$total_headers","
+
+		fi
+
+
+
+		total_headers=$total_headers"group_sudo_privileges"
+		chk_sudo_groups
+
+	fi
+
+
+	build_headers $total_headers
+
+	
+	gen_output $CSV_FILE_OUT
+
+
+}
+
+
+
+
+
+function print_help()
+{
+
+	echo "Check sudo groups, users and process filter by users"
+	echo -e "Version 1.0, Jan-2018."
+	echo -e "\r\n"
+	#echo -e "Support: rmoreno@protonmail.com\r\n"
+	echo -e "  Usage: $0 -g -p -u -o logs.csv\r\n"	
+	echo -e "\t -g\t\t Enumerate all groups allow sudo privileges"
+	echo -e "\t -u pattern\t Enumerate all processs of a specific user"
+	echo -e "\t -p\t\t Check local users sudo privileges"
+	echo -e "\t -o file\t Output log file"
+	echo -e "\t -h\t\t Print help"
+	echo -e "\r\n"
+
+}
+
+
+
+function chk_user_exist()
+{
+
+	if getent passwd $1 > /dev/null 2>&1; then
+	    USER_EXIST=1
+	else
+	    USER_EXIST=0
+	fi
+
+
+}
+
+
+
+function gen_output()
+{
+
+
+
+	len_users=${#SUDO_USERS_FOUND[@]}
+	#echo "[+] Users founds:: " $len_users
+	
+	len_procs=${#PROCS2[@]}
+	#echo "[+] Process founds:: " $len_procs
+
+	len_homes=${#GROUPS_SUDO_FOUND[@]}
+	#echo "[+] /home/ founds:: " $len_homes
+
+
+	max_values=$len_users
+
+
+	if [ $max_values -lt $len_procs ]; then
+		max_values=$len_procs
+	fi
+
+
+	if [ $max_values -lt $len_homes ]; then
+		max_values=$len_homes
+	fi
+
+
+
+	index=0
+	let max_values--
+
+
+
+
+	while [ $index -le $max_values ]
+	do
+
+
+		# Procs privileges
+
+		if [ $param_users -eq 1 ]
+		then    	
+			str_out_csv="${PIDS2[$index]}"
+			str_out_csv=$str_out_csv","
+			str_out_csv=$str_out_csv"${PROCS2[$index]}"
+			str_out_csv=$str_out_csv","
+			str_out_csv=$str_out_csv"${USER2[$index]}"		
+		fi		
+
+
+
+
+		# Homes users sudo priveleges (by home)
+
+		if [ $param_privileges -eq 1 ]
+		then    	
+
+
+
+		if [ $param_users -eq 1 ]
+		then
+
+			str_out_csv="$str_out_csv,"
+
+		fi
+
+
+
+
+
+			str_out_csv="$str_out_csv${SUDO_USERS_FOUND[$index]}"
+			#echo $str_out_csv
+
+
+		if [ $param_group -eq 1 ]
+		then
+
+			str_out_csv="$str_out_csv,"
+
+		fi
+
+		fi
+
+
+
+
+
+		# Groups sudo privileges
+
+		if [ $param_group -eq 1 ]
+		then
+
+		str_out_csv=$str_out_csv"${GROUPS_SUDO_FOUND[$index]}"		
+
+		fi
+
+
+
+		
+		echo $str_out_csv >> $1
+		str_out_csv=""
+		((index++))
+
+	done
+
+}
+
+
+
+
+
+
+
+function extract_group_sudoers()
+{
+
+
+
+while read -r line; do	
+
+
+		ret_awk=$(awk '{ 
+
+					match($0, /^%[a-z]*/)
+					{
+						print substr($0, RSTART+1, RLENGTH-1)
+					}
+
+				}'  <<< $line)
+		
+
+
+		size=${#ret_awk}
+
+		if (( $size != 0 )); then
+			
+	    	GROUPS_SUDO_FOUND[$QUANTITY_SUDO_GROUPS]=$ret_awk
+	    	let QUANTITY_SUDO_GROUPS++
+
+		fi
+
+
+done < "$1"
+
+
+}
+
+
+
+
+function build_headers()
+{	
+
+	if [ $total_flags -eq 1 ]
+	then    	
+	  	echo -e "\r\n[!] Please specific action\r\n"
+		print_help	
+		exit -1
+
+	fi		
+	
+
+
+	echo $1 > $CSV_FILE_OUT
+
+}
+
+
+
 
 
 function parse_process_output()
 {
 	USER=$1
 	PROCS=$2
-	PID=$3
+	PIDS=$3
 }
 
 
@@ -65,54 +415,48 @@ function gen_csv_sudoers_groups()
 
 
 
-function gen_csv_sudoers_by_home()
+function order_procs()
 {
 
-
-	echo "$1" > $3
-
-
-
-	len=${#POINTER[@]}
- 
-
-	for (( i=1; i<$len; i++ )); 
-		do
-
-
-			user_found_sudoers "${POINTER[$i]}" $2			
-
-
-			if (( $RET_FOUND_SUDOERS_FILE == 1 )); then
-			  echo "${POINTER[$i]}" >> $3
-
-			fi
-
-
-
-		done
-	
-
-}
-
-
-function gen_csv_procs()
-{
-
-	echo "$1,$2,$3" > $5
+	LEN_PROCS=0
 
 
 	while IFS= read -r line
 	do
 
 	    parse_process_output $line
-	    echo "$PID,$PROCS,$USER" >> $5
+
+		PROCS2[LEN_PROCS]=$PROCS
+		USER2[LEN_PROCS]=$USER
+		PIDS2[LEN_PROCS]=$PIDS
+
+	    let LEN_PROCS++	    	   
 
 
 	done <<< "$POINTER"
 
+	
+	# len=${#local_procs[@]}
+	# echo "length::" $len
+	# PROCS=$local_procs
+
+	# len3=${#local_pids[@]}
+	# echo "length pids::" $len3
+	# PIDS=$local_pids
+
+
+	# len2=${#local_users[@]}
+	# echo "length user::" $len2
+	# USER=$local_users
+
+
+	# len=${#local_procs[@]}
+	# len=${#PROCS2[@]}
+	# echo $len
 
 }
+
+
 
 
 function parser_process()
@@ -135,6 +479,7 @@ function running_process_by_user()
 
 	process_by_user=`ps -eo user,comm,pid | grep $1`
 	POINTER=$process_by_user
+	order_procs
 
 }
 
@@ -159,29 +504,35 @@ function user_found_sudoers()
 
 
 
-function chk_admin_access()
+function chk_sudo_by_home()
 {
 
-	INC=0
+	for userlist in `ls -F $1`; do
 
-	for entry in $1*
-	do
 
-		let INC++
-		
-		POINTER[$INC]=$(awk '{
-		  		n = split($0, t, "/")
 
-		  		for (i = 0; ++i <= n;)
+	    userlist=${userlist::-1}
 
-					if (i==3)		  			
-		    			print t[i]
+	    chk_user_exist $userlist
 
-		  }'  <<< $entry)
 
-		 		
 
-	done	
+
+	    if (( $USER_EXIST == 0 )); then
+	    	continue
+	    fi
+
+
+
+		if `sudo -U $userlist -l > /dev/null`; then
+			#echo "Sudo access for " $userlist			
+			SUDO_USERS_FOUND[$QUANTITY_SUDO_USERS]=$userlist
+			let QUANTITY_SUDO_USERS++
+
+		fi		
+
+	done
+
 }
 
 
@@ -193,41 +544,112 @@ function chk_sudo_groups()
 {
 
 
-	POINTER=$(awk '{ 
 
-			if (index($0,"%")==1)
-			{ 
-				print $0
-			} 
-
-		 }' $1)
+	len=${#SUDOERS_FOLDERS[@]}
 
 
+
+
+	for (( i=0; i<$len; i++ )); 
+		do			
+
+			for f in `ls -A "${SUDOERS_FOLDERS[$i]}"`; do				
+
+  				extract_group_sudoers "${SUDOERS_FOLDERS[$i]}$f"
+
+			done
+
+
+		done
 
 }
-
-
-
-
-running_process_by_user $FILTER_ACCOUNT_NAME
-gen_csv_procs "pid" "proc_name" "account_name" $FILTER_ACCOUNT_NAME "/tmp/sudoers_svc.csv"
-
 
 
 chk_is_root
 
 
-
-chk_sudo_groups "/etc/sudoers"
-gen_csv_sudoers_groups "groups_name" "/etc/sudoers" "/tmp/sudoers_groups.csv"
+argc="$#"
 
 
+for i in "$@"
+do
 
 
-chk_admin_access "/home/"
-gen_csv_sudoers_by_home "account_name" "/etc/sudoers" "/tmp/sudoers_home.csv"
+	if (( $next_user_filter == 1 )); then
+		next_user_filter=0
+		FILTER_ACCOUNT_NAME=$i
+	fi
 
 
 
-running_process_by_user $FILTER_ACCOUNT_NAME
-gen_csv_procs "pid" "proc_name" "account_name" $FILTER_ACCOUNT_NAME "/tmp/sudoers_svc.csv"
+	if [ $next_filename == 1 ]; then
+
+
+		next_filename=0
+
+		echo > $i > /dev/null
+	    
+
+		if [ ! -e $i ]; then
+		    echo "[!] Unable create file log"
+		    exit -2
+
+			else
+				CSV_FILE_OUT=$i
+
+		fi 		
+
+	fi 	
+
+
+
+
+case $i in
+
+
+    -g)
+    param_group=1
+    ;;
+
+
+
+    -u)
+    param_users=1
+    next_user_filter=1
+    ;;
+
+
+
+    -p)	
+    param_privileges=1    
+    ;;
+
+
+
+    -o)
+    param_output=1
+    next_filename=1
+    ;;
+
+
+    -h)
+    param_help=1
+    ;;
+
+
+esac
+done
+
+
+#echo $param_group
+#echo $param_users
+#echo $param_privileges
+#echo $param_help
+#echo $param_output
+#exit
+
+
+total_flags=$(( param_help + param_output + param_group + param_users + param_privileges))
+
+parse_command_line 
+
